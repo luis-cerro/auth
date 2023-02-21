c1a5047c957cd2c42fd0c231ea45ec0559bc7850
Luis Cerro <luis.cerro@kellogg.northwestern.edu>
2023-02-21 22:02:18 +0000

    commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+
+    def new
+    end
+
+    def create
+        #check if user exists
+        @user = User.find_by({"email" => params["email"]})
+        if @user
+        #if they do, check that they know their password
+            if BCrypt::Password.new(@user["password"]) == params["password"]
+                redirect_to "/companies"
+            else
+                redirect_to "/sessions/new"
+            end
+        else
+            redirect_to "/sessions/new"
+        end
+    end
+end
